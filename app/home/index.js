a1710d34c707a7e2d96ae01216b6893ba135b4b9
NUR AULIYAH <149492209+Aulliyah30@users.noreply.github.com>
2023-12-08 18:24:26 +0700

    Delete app directory

--- app/home/index.js
+++
@@ -1,7 +0,0 @@
-const today = new Date();
-const day = today.toLocaleString('default', { weekday: 'long' });
-const month = today.toLocaleString('default', { month: 'long' });
-const date = today.getDate();
-const year = today.getFullYear();
-const dateElement = document.getElementById('date');
-dateElement.innerHTML = `${day} ${month} ${date}, ${year}`;
